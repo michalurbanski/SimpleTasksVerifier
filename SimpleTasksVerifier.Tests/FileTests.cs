5bca703683da60f4462e6a7366c30bce38011095
michal_urbanski <michal_urbanski@windowslive.com>
2015-11-07 22:39:28 +0100

    First unit test checks current file retrieval

---
+++ SimpleTasksVerifier.Tests/FileTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace SimpleTasksVerifier.Tests
+{
+    [TestClass]
+    public class FileTests
+    {
+        [TestMethod]
+        public void Test_Get_Current_FileName_ShouldSucceed()
+        {
+            // Arrange
+            string expected = "FileTests.cs";
+
+            // Act 
+            string fullFilePath = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
+            string fileName = Path.GetFileName(fullFilePath); 
+
+            // Assert
+            Assert.AreEqual(expected, fileName); 
+        }
+
+        [TestMethod]
+        public void Test_Check_If_File_Exists_ExistingFile()
+        {
+            // Arrange
+            string path = ""; 
+
+            // Act 
+
+            // Assert
+
+            Assert.Inconclusive(); 
+        }
+
+        [TestMethod]
+        public void Test_Check_If_File_Exists_NotExistingFile()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+
+            Assert.Inconclusive(); 
+        }
+    }
+}
