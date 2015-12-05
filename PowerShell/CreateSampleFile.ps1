0ebe8d1aa7938648c1020dc24a3faee696b635a2
michal_urbanski <michal_urbanski@windowslive.com>
2015-12-05 20:25:19 +0100

    PowerShell to create file updated

--- PowerShell/CreateSampleFile.ps1
+++ PowerShell/CreateSampleFile.ps1
@@ -2,7 +2,16 @@
 
 
 write-host $env:APPDATA
+$applicationName = "SimpleTasksVerifier"
+$fileName = "SampleDataFile.txt"
 
+$path = "$env:APPDATA\$applicationName\"
 
-
-Test-Path $env:APPDATA\SimpleTaskVerifier
+if(Test-Path $path\$fileName){
+    write-host "File $fileName is already created"
+}
+else{
+    write-host "Creating file $fileName..."
+    New-Item -Path $env:APPDATA -Name $applicationName -ItemType Directory
+    New-Item -Path $path -Name $fileName -ItemType File 
+}
