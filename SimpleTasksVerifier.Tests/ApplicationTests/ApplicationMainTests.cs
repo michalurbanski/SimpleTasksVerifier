f058e0ff5bc4a2563624bf537c02a1b827329d62
michalurbanski <michal_urbanski@windowslive.com>
2016-01-05 22:21:34 +0100

    Adds class for handling main application logic and tests for new concept

---
+++ SimpleTasksVerifier.Tests/ApplicationTests/ApplicationMainTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleTasksVerifier.Logic;
+using SimpleTasksVerifier.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleTasksVerifier.Tests.ApplicationTests
+{
+    [TestClass]
+    public class ApplicationMainTests
+    {
+        [TestMethod]
+        public void Test_ValidFile_ShouldReturnAnyResults()
+        {
+            string filePath = string.Empty; 
+            ApplicationLogic application = new ApplicationLogic(filePath);
+
+            application.ReadFile();
+            application.ProcessFile();
+            IEnumerable<TaskResult> results = application.GetResults();
+
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results.Any());
+        }
+    }
+}
---
+++ SimpleTasksVerifier/Logic/ApplicationLogic.cs
@@ -0,0 +1,33 @@
+﻿using SimpleTasksVerifier.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTasksVerifier.Logic
+{
+    public class ApplicationLogic
+    {
+        private string _filePath;
+
+        public ApplicationLogic(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void ReadFile()
+        {
+        }
+
+        public void ProcessFile()
+        {
+            
+        }
+
+        public IEnumerable<TaskResult> GetResults()
+        {
+            return new List<TaskResult> { new TaskResult() };
+        }
+    }
+}
